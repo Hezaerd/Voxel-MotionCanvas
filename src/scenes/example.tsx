ccf963adc8b69d8a8b29e1b80e1b64992a687e0d
Swann <swann.rouanet@gmail.com>
2024-05-05 21:52:37 +0200

    Experiencing with an inheritence demo

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,59 +1,89 @@
-import {CODE, Code, Rect, lines, makeScene2D, word} from '@motion-canvas/2d';
-import {
-  DEFAULT,
-  SimpleSignal,
-  createRef,
-  createSignal,
-  easeOutCubic,
-  waitFor,
-} from '@motion-canvas/core';
+import {makeScene2D, Code} from '@motion-canvas/2d';
+import {DEFAULT, all, beginSlide, createRef, waitFor} from '@motion-canvas/core';
 
+import { DARK_GRAY } from '../utils/colors';
+import { CppCode } from '../nodes/CppCode';
 
 export default makeScene2D(function* (view) {
-  const codeRef = createRef<Code>();
+  const code = createRef<Code>();
 
-  const radius = createSignal(1);
-  const r2 = createSignal(() => radius() * radius());
-  const area = createSignal(() => Math.PI * r2());
-
-  const functionName = Code.createSignal('calculateArea');
-
-  const displayFloat = (signal: SimpleSignal<number, void>) => {
-    return () => signal().toFixed(2).toString();
-  };
-
-  view.add(
-    <Code
-      fontSize={42}
-      ref={codeRef}
-      code={CODE`\
-const radius = ${displayFloat(radius)};
-area = ${functionName}(radius); // ${displayFloat(area)}
-
-const ${functionName} = () => {
-  const r2 = radius * radius; // ${displayFloat(radius)} * ${displayFloat(
-    radius,
-  )} = ${displayFloat(r2)}
-  return Math.PI * r2;
+  const animalClass = Code.createSignal(`
+class IAnimal {  
+  private:
+    string name;
 };
-    `}
-    />,
+    
+`);
+  
+    const dogClass = Code.createSignal(`
+class Dog : public IAnimal {
+  public:
+    Dog(string name) {
+      this->name = name;
+    }
+};
+    
+`);
+
+  view.fill(DARK_GRAY);
+  view.add(
+    <CppCode 
+      ref={code}
+      fontSize={32}
+      offsetX={-1}
+      x={-400}
+      />
   );
 
-  yield* waitFor(2);
-  yield* codeRef().selection(codeRef().findAllRanges(/radius/gi), 0.8);
+  yield* beginSlide('Animal interface');
+  yield* code().code.append(animalClass, 0.6);
+
+  yield* beginSlide('Dog class')
+  yield* code().code.append(dogClass, 0.6);
+
+  yield* beginSlide('Speak virtual')
+  yield* all(animalClass(`
+class IAnimal {
+  public:
+    virtual void Speak() = 0;
+  
+  private:
+    string name;
+};
+    
+`, 0.6));
+
+  yield* beginSlide('virtual selection')
+  yield* code().selection(code().findFirstRange('virtual'), 0.6);
+
+  yield* beginSlide('= 0 selection')
+  yield* code().selection(code().findFirstRange('= 0'), 0.6);
+  
+  yield* beginSlide('Speak override')
+  yield* code().selection(DEFAULT, 0.6);
+  yield* all(dogClass(`
+class Dog : public IAnimal {
+  public:
+    Dog(string name) {
+      this->name = name;
+      Speak();
+    }
+
+    void Speak() override {
+      std::cout << "Woof!" << endl;
+    }
+};
+      
+`, 0.6));
+
+  yield* beginSlide('Speak override selection')
+  yield* code().selection(code().findFirstRange('override'), 0.6);
+
+  yield* beginSlide('woof selection')
+  yield* code().selection(code().findFirstRange('"Woof!"'), 0.6);
+
+  yield* beginSlide('clear selection')
+  yield* code().selection(DEFAULT, 0.6);
+
   yield* waitFor(1);
-  yield* codeRef().selection(DEFAULT, 0.8);
-  yield* functionName('getAreaFromRadius', 1);
-  yield* waitFor(2);
-  yield* radius(10, 3, easeOutCubic);
-  yield* waitFor(2);
-  yield* codeRef().code.replace(
-    lines(4, 5),
-    CODE`  return Math.PI * radius * radius;\n`,
-    1,
-  );
-  yield* waitFor(1);
-  yield* codeRef().code.remove(word(4, 1, 7), 1);
-  yield* waitFor(2);
 });
